fa29cfe1f2f1af62986e4c911c915174d1bd0dc2
zagart <zagart.dev@mail.ru>
2024-12-21 19:37:32 +0400

    refactor screen ext & add kdoc

--- app/src/main/kotlin/com/zagart/navigation/template/navigation/NavigationExtensions.kt
+++ app/src/main/kotlin/com/zagart/navigation/template/navigation/NavigationExtensions.kt
@@ -2,7 +2,13 @@ package com.zagart.navigation.template.navigation
 
 import android.os.Bundle
 import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.SizeTransform
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavBackStackEntry
+import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -12,17 +18,52 @@ import com.zagart.navigation.template.presentation.navigation.Destination
 import com.zagart.navigation.template.presentation.navigation.NavigationViewModel
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
+import kotlin.reflect.KType
 import kotlin.reflect.typeOf
 
 val defaultTypeMap = mapOf(
     typeOf<Destination.Args>() to DestinationArgsNavType
 )
 
+/**
+ * Wrapper around [composable]. It keeps [NavigationViewModel.currentDestinationState] updated, so
+ * it always should be used instead of [composable].
+ * In addition, provides [NavBackStackEntry] to [Destination] conversion.
+ */
 inline fun <reified T : Destination> NavGraphBuilder.screen(
+    typeMap: Map<KType, @JvmSuppressWildcards NavType<*>> = defaultTypeMap,
+    deepLinks: List<NavDeepLink> = emptyList(),
+    noinline enterTransition:
+    (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards
+    EnterTransition?)? =
+        null,
+    noinline exitTransition:
+    (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards
+    ExitTransition?)? =
+        null,
+    noinline popEnterTransition:
+    (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards
+    EnterTransition?)? =
+        enterTransition,
+    noinline popExitTransition:
+    (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards
+    ExitTransition?)? =
+        exitTransition,
+    noinline sizeTransform:
+    (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards
+    SizeTransform?)? =
+        null,
     noinline content: @Composable AnimatedContentScope.(T) -> Unit
 ) {
-
-    composable<T>(typeMap = defaultTypeMap) { entry ->
+    composable<T>(
+        typeMap = typeMap,
+        deepLinks = deepLinks,
+        enterTransition = enterTransition,
+        exitTransition = exitTransition,
+        popEnterTransition = popEnterTransition,
+        popExitTransition = popExitTransition,
+        sizeTransform = sizeTransform,
+    ) { entry ->
         val destination = entry.toRoute<T>()
         NavigationViewModel.currentDestinationState.value = destination
 
